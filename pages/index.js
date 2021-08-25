f20ddb305d9854226ef6ace6faa6a5ea7aa47ed0
mateus-cirino <mateus.silva@projetusti.com.br>
2021-08-24 22:43:28 -0300

    feature/adicao-botao-close-modal

--- pages/index.js
+++ pages/index.js
@@ -52,6 +52,7 @@ const Home = () => {
                 marginLeft="auto"
                 backgroundColor="#FFFFFF"
               >
+                { propsDoModal.CloseButton }
                 <Form />
               </BoxStyled>
             )
--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Image from 'next/image';
 import { LockScrollGlobalStyle } from '../../theme';
 import ModalMotion from './motion';
 import ModalWrapperStyled from './style';
+import closeIcon from '../../../public/pictures/close-icon.svg';
+import ButtonStyled from '../button/style';
 
 const Modal = ({ isOpen, onClose, children }) => {
   const onClickModal = (event) => {
@@ -12,12 +15,19 @@ const Modal = ({ isOpen, onClose, children }) => {
     }
   };
 
+  const CloseButton = (
+    <ButtonStyled ghost display="flex" marginLeft="auto" onClick={onClose}>
+      <Image src={closeIcon} alt="Icone para fechar o modal" />
+    </ButtonStyled>
+  );
+
   return (
     <ModalWrapperStyled isOpen={isOpen} onClick={onClickModal}>
       <ModalMotion isOpen={isOpen}>
         { isOpen && <LockScrollGlobalStyle /> }
         {children({
           'data-modal-safe-area': 'true',
+          CloseButton,
         })}
       </ModalMotion>
     </ModalWrapperStyled>
